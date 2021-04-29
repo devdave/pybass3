f213646b41c78b50cd922433a436149624ef5ec4
devdave <devdave@ominian.net>
2021-04-29 17:46:14 -0600

    too many changes to comment, I am losing control of this file and its becoming a big ball of mud

--- pybass3/playlist.py
+++ pybass3/playlist.py
@@ -3,6 +3,7 @@ import enum
 from pathlib import Path
 import random
 
+from .bass_module import BassException
 from .song import Song
 
 try:
@@ -26,6 +27,9 @@ class PlaylistMode(enum.Enum):
 
     sequential = enum.auto()
     random = enum.auto()
+
+    progress_on_error = enum.auto()
+    die_on_error = enum.auto()
 
 class Playlist:
 
@@ -52,6 +56,8 @@ class Playlist:
         self.mode = PlaylistMode.sequential
         self.play_mode = PlaylistMode.one_time
 
+        self.error_mode = PlaylistMode.progress_on_error
+
         self.queue_position = None
         self.current_song = None
 
@@ -74,8 +80,11 @@ class Playlist:
 
     def add_song(self, song_path):
         song = self.song_cls(song_path)
+        song.duration
+        song.free_stream()
         self.songs.append(song)
-        return len(self.songs), song
+        self.queue.append(len(self.songs)-1)
+        return len(self.songs)-1, song
 
     def add_directory(self, dir_path: Path, recurse=True):
         files = (file for file in dir_path.iterdir() if file.is_file() and file.suffix in self.VALID_TYPES)
@@ -196,33 +205,68 @@ class Playlist:
 
 
     def next(self):
+        try:
+            return self._next()
+        except BassException as bexc:
+            if self.error_mode == PlaylistMode.progress_on_error:
+                self.queue_position += 1
+                if self.queue_position > len(self.queue):
+                    self.queue_position = 0
+
+                return self._next()
+
+    def _next(self):
 
         if self.fadein_song is not None:
             self.current.stop()
             self.current = self.fadein_song
             self.fadein_song = None
-            self.queue_position += 1
+
         else:
             self.current.free_stream()
             self.current = self.upcoming
-            self.queue_position += 1
             if self.current is not None:
                 self.current.play()
 
+        self.queue_position += 1
+        if self.queue_position > len(self.queue):
+            self.queue_position = 0
+
+        song_id = self.queue[self.queue_position]
+        return song_id, self.current
 
     def previous(self):
+        try:
+            return self._previous()
+        except BaseException:
+            if self.error_mode == PlaylistMode.progress_on_error:
+                self.queue_position -= 1
+                if self.queue_position < 0:
+                    self.queue_position = len(self.queue)
+
+                return self._previous()
+            else:
+                raise
+
+
+    def _previous(self):
         prior = self.prior
         if self.fadein_song is not None:
-            self.fadein_song.stop()
+            self.fadein_song.free_stream()
+            self.fadein_song = None
             self.current.move2position_seconds(0)
             self.current.play()
-            self.fadein_song = None
         else:
-            self.current.stop()
             self.current_song.free_stream()
             self.current_song = prior
             self.current_song.play()
 
+            self.queue_position -= 1
+            if self.queue_position < 0:
+                self.queue_position = len(self.queue)
+
+        song_id = self.queue[self.queue_position]
+        return song_id, self.current
 
     def tick(self):
         current_pos = self.current.position_bytes
@@ -253,7 +297,7 @@ class Playlist:
             self.current.play()
 
     def items(self):
-        for song_id, song in self.songs:
+        for song_id, song in enumerate(self.songs):
             yield song_id, song
 
     def __len__(self):
@@ -268,39 +312,63 @@ if HAS_PYSIDE2:
 
     from .song import QtSong
 
-    class QtPlaylist(Playlist):
+    class QtPlaylist(QtCore.QObject, Playlist):
 
-        song_added = QtCore.Signal(int, Song)
-        song_changed = QtCore.Signal(int, Song)  # Song ID and Song object
-        music_paused = QtCore.Signal()
-        music_playing = QtCore.Signal()
-        music_stopped = QtCore.Signal()
+        song_added = QtCore.Signal(int) # Song ID, Qt DOES NOT like when I try to pass the Song object
+        song_changed = QtCore.Signal(int)  # Song ID
+        music_paused = QtCore.Signal(int)
+        music_playing = QtCore.Signal(int)
+        music_stopped = QtCore.Signal(int)
 
         timer: QtCore.QTimer
 
         def __init__(self):
-            super(QtPlaylist, self).__init__(QtSong)
+            QtCore.QObject.__init__(self)
+            Playlist.__init__(self, QtSong)
 
+            self.timer = QtCore.QTimer()
+            self.timer.setInterval(500)
+            self.timer.timeout.connect(self.tick)
 
         def add_song(self, song_path):
-            index, song = super(QtPlaylist, self).add_song(song_path)
+            return super(QtPlaylist, self).add_song(song_path)
 
-            self.song_added.emit(index, song)
+            # self.song_added.emit([index, song])
 
         def play(self):
             super(QtPlaylist, self).play()
             if self.current.is_playing:
-                self.music_playing.emit()
+                self.music_playing.emit(self.song_id)
+                self.timer.start()
+
+        def play_song(self, song_id):
+            if self.fadein_song is not None:
+                self.fadein_song.free_stream()
+                self.fadein_song = None
+
+            if self.current is not None:
+                self.current.free_stream()
+                self.current = None
+
+            self.current = self.songs[song_id]
+            self.current.play()
+
+            self.music_playing.emit(song_id)
+            self.timer.start()
 
         def stop(self):
             super(QtPlaylist, self).stop()
             if self.current.is_stopped:
-                self.music_stopped.emit()
+                self.music_stopped.emit(self.song_id)
+
+            self.timer.stop()
 
         def pause(self):
             super(QtPlaylist, self).pause()
             if self.current.is_paused:
-                self.music_paused.emit()
+                self.music_paused.emit(self.song_id)
+
+            self.timer.stop()
 
 
 
