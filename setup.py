b1806f962241c2e38c782e933112302a4256191c
devdave <devdave@ominian.net>
2021-05-01 20:44:17 -0600

    Rely more on the setup config file over setup.py file

--- setup.py
+++ setup.py
@@ -6,26 +6,6 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="PyBASS3",
-    version="0.0.1",
-    author="DevDave",
-    author_email="devdave@ominian.net",
-    description="A Python 3 compatible, managed, wrapper around the c BASS Audio library",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/devdave/pybass3",
-    classifiers = [
-        "Programming Language :: Python :: 3",
-        "Operating System :: Microsoft :: Windows",
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: Free for non-commercial use",
-        "Natural Language :: English",
-        "Topic :: Multimedia :: Sound/Audio",
-
-
-    ],
-    packages=find_packages("src"),
-    package_dir={"": "src"},
     package_data={
         "pybass3": ["vendor/*.dll", "vendor/*.h", "vendor/*.txt"],
     },
