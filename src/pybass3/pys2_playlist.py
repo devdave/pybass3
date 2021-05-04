9cb52ea31e2470c8b893e77cec00697b0db2ee4d
devdave <devdave@ominian.net>
2021-05-04 07:17:42 -0600

    Added in logging to aid debugging

--- src/pybass3/pys2_playlist.py
+++ src/pybass3/pys2_playlist.py
@@ -3,14 +3,15 @@
 
 
 """
+import logging
+
 from PySide2 import QtCore
 
 Qt = QtCore.Qt
 
 from .pys2_song import Pys2Song
 
-from .playlist import Playlist
-
+log = logging.getLogger(__name__)
 
 class Pys2Playlist(QtCore.QObject, Playlist):
     song_added = QtCore.Signal(str)  # Song ID, Qt DOES NOT like when I try to pass the Song object
@@ -24,18 +25,23 @@ class Pys2Playlist(QtCore.QObject, Playlist):
     def __init__(self):
         QtCore.QObject.__init__(self)
         Playlist.__init__(self, Pys2Song)
+        log.debug("Initialized playlist: Precision is %s", tick_precision)
 
 
 
     def add_song(self, song_path):
         return super(Playlist, self).add_song(song_path)
+        log.debug("Pys2Playlist.add_song %s", song_path)
 
     def play(self):
         super(QtPlaylist, self).play()
         if self.current.is_playing and self.current is not None:
+        log.debug("Pys2Playlist.play self.current is %s", self.current)
             self.music_playing.emit(self.current.id)
 
     def play_song(self, song_id):
+        log.debug("Pys2Playlist.play_song %s", song_id)
+
         if self.fadein_song is not None:
             self.fadein_song.free_stream()
             self.fadein_song = None
@@ -52,13 +58,16 @@ class Pys2Playlist(QtCore.QObject, Playlist):
     def stop(self):
         super(Playlist, self).stop()
         if self.current.is_stopped and self.current is not None:
+        log.debug("Pys2Playlist.stop called")
             self.music_stopped.emit(self.current.id)
 
     def pause(self):
         super(Playlist, self).pause()
         if self.current.is_paused and self.current is not None:
+        log.debug("Pys2Playlist.pause called")
             self.music_paused.emit(self.current.id)
 
+        log.debug("Pys2Playlist.previous")
 
 
 
