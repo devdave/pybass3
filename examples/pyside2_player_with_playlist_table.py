b0950b38a09aa5fd834aea56a7d103fd3cac54ed
devdave <devdave@ominian.net>
2021-05-04 19:51:30 -0600

    Copied over Pyside2 playlist example code to save time

--- examples/pyside2_player_with_playlist_table.py
+++ examples/pyside2_player_with_playlist_table.py
@@ -1,299 +1,344 @@
-import pathlib
 import typing
-import logging
 import sys
+import pathlib
+import time
+import logging
 
-import PySide2
-from PySide2 import QtWidgets
+
 from PySide2 import QtCore
-
+from PySide2 import QtWidgets
 Qt = QtCore.Qt
 
+from pybass3 import BassException
 from pybass3.pys2_playlist import Pys2Playlist
 from pybass3.pys2_song import Pys2Song
-from pybass3 import BassException
+
 
 log = logging.getLogger(__name__)
 
-class PlaylistModel(QtCore.QAbstractTableModel):
+
+class PlayerWindow(QtWidgets.QMainWindow):
+
+    pl_position: QtWidgets.QLabel
+    song_title: QtWidgets.QLabel
+    song_position: QtWidgets.QLabel
+    song_duration: QtWidgets.QLabel
+    seek_bar: QtWidgets.QSlider
+    prv_btn: QtWidgets.QPushButton
+    ply_btn: QtWidgets.QPushButton
+    pause_btn: QtWidgets.QPushButton
+    stop_btn: QtWidgets.QPushButton
+    next_btn: QtWidgets.QPushButton
+    add_file_btn: QtWidgets.QPushButton
+    add_dir_btn: QtWidgets.QPushButton
+    randomize_btn: QtWidgets.QPushButton
+    sequential_btn: QtWidgets.QPushButton
+    pl_table: QtWidgets.QTableView
 
 
+    def __init__(self, *args, **kwargs):
+        super(PlayerWindow, self).__init__(*args, **kwargs)
+        self.pl_position = None
+        self.song_title = None
+        self.song_position = None
+        self.song_duration = None
+        self.seek_bar = None
+        self.prv_btn = self.ply_btn = self.pause_btn = self.stop_btn = self.next_btn = None
+        self.add_file_btn = self.add_dir_btn = None
+        self.randomize_btn = self.sequential_btn = None
+        self.pl_table = None
+        self.setupUI()
 
-    record_added = QtCore.Signal(int)
+        log.debug("Initialized PlayerWindow")
+
+    def setupUI(self):
+        frame = QtWidgets.QFrame()
+        vbox = QtWidgets.QVBoxLayout(frame)
+
+        self.pl_position = QtWidgets.QLabel("Playlist position")
+        vbox.addWidget(self.pl_position)
+
+        self.song_title = QtWidgets.QLabel("Song: ")
+        vbox.addWidget(self.song_title)
+
+        time_line_box = QtWidgets.QHBoxLayout()
+
+        self.song_position = QtWidgets.QLabel("00:00")
+        time_line_box.addWidget(self.song_position)
+
+        self.song_duration = QtWidgets.QLabel("99:99")
+        time_line_box.addWidget(self.song_duration)
+
+        vbox.addLayout(time_line_box)
+
+        self.seek_bar = QtWidgets.QSlider(Qt.Horizontal)
+        vbox.addWidget(self.seek_bar)
+
+        control_box = QtWidgets.QHBoxLayout(frame)
+
+        self.prv_btn = QtWidgets.QPushButton("Previous")
+        self.ply_btn = QtWidgets.QPushButton("Play")
+        self.pause_btn = QtWidgets.QPushButton("Pause")
+        self.stop_btn = QtWidgets.QPushButton("Stop")
+        self.next_btn = QtWidgets.QPushButton("Next")
+
+        for button in [self.prv_btn, self.ply_btn, self.pause_btn, self.stop_btn, self.next_btn]:
+            control_box.addWidget(button)
+        vbox.addLayout(control_box)
+
+        add_box = QtWidgets.QHBoxLayout(frame)
+
+        self.add_file_btn = QtWidgets.QPushButton("Add file")
+        self.add_dir_btn = QtWidgets.QPushButton("Add directory")
+
+        for button in [self.add_file_btn, self.add_dir_btn]:
+            add_box.addWidget(button)
+
+        vbox.addLayout(add_box)
+
+        queue_box = QtWidgets.QHBoxLayout()
+        self.randomize_btn = QtWidgets.QPushButton("Randomize")
+        self.sequential_btn = QtWidgets.QPushButton("Sequential")
+        queue_box.addWidget(self.randomize_btn)
+        queue_box.addWidget(self.sequential_btn)
+
+        vbox.addLayout(queue_box)
 
 
+        frame.setLayout(vbox)
+        self.setCentralWidget(frame)
 
-    def __init__(self, playlist: Pys2Playlist):
-        super(PlaylistModel, self).__init__()
+        log.debug("PlayerWindow - UI is setup")
+
+
+class PlaylistTableModel(QtCore.QAbstractTableModel):
+
+    def __init__(self, playlist):
         self.playlist = playlist
-        log.debug("initialized")
 
-
-
-    def headerData(self, section:int, orientation:QtCore.Qt.Orientation, role: int = ...) -> typing.Any:
-        # log.debug("headerData called with %s, %s, %s", section, orientation, role)
-        if role == Qt.DisplayRole:
-            if section == 0:
-                return "RID"
-            elif section == 1:
-                return "Length"
-            elif section == 2:
-                return "Name"
-
-    def columnCount(self, parent: QtCore.QModelIndex = ...) -> int:
-        """
-        Song ID, Song length, Song file name
-
-        :param parent:
-        :return:
-        """
-        return 3
-
-    def rowCount(self, parent:PySide2.QtCore.QModelIndex=...) -> int:
-        # log.debug("rowCount has #%s rows", len(self.playlist))
+    def rowCount(self, parent:QtCore.QModelIndex = ...) -> int:
         return len(self.playlist)
 
-    def data(self, index:QtCore.QModelIndex, role:int=...) -> typing.Any:
-        row = index.row()
-        col = index.column()
+    def columnCount(self, parent:QtCore.QModelIndex = ...) -> int:
+        return 3
 
+    def headerData(self, section:int, orientation:QtCore.Qt.Orientation, role: int = ...) -> typing.Any:
+        if role == Qt.DisplayRole:
+            if section == 0:
+                return "SID"
+            elif section == 1:
+                return "Name"
+            elif section == 2:
+                return "Time"
+
+        return None
+
+    def data(self, index:QtCore.QModelIndex, role: int = ...) -> typing.Any:
+
+        song = self.playlist.get_song_by_row(index.row())
+        col = index.column()
         if role == Qt.DisplayRole:
             if col == 0:
-                return row
-            else:
-                song = self.playlist.songs[row]
-                if col == 1:
-                    return song.duration
-                elif col == 2:
-                    return song.file_path.name
-        elif role == Qt.ToolTipRole:
-            if col == 2:
-                song = self.playlist.songs[row]
-                return song.file_path.as_posix()
-
-    def add_song(self, song_id, song, play_on_add=True):
-        log.debug("Adding song %s - %s", song_id, song)
-        rowCount = song_id
-        last = rowCount
-        self.beginInsertRows(QtCore.QModelIndex(), rowCount, last)
-
-        self.endInsertRows()
-        if play_on_add is True:
-            self.playlist.play_song(song_id)
-
-        self.record_added.emit(song_id)
+                return song.id
+            elif col == 1:
+                song_name = song.file_path.name
+                return song_name
+            elif col == 2:
+                return song.duration_time
 
 
 
 
-class PlayerControl(QtCore.QObject):
 
-    playlist: Pys2Playlist
-    model: PlaylistModel
-    progress_mousedown: bool
-
+class PlayerController(QtCore.QObject):
 
     def __init__(self):
-        super(PlayerControl, self).__init__()
+        super(PlayerController, self).__init__()
+        self.view = PlayerWindow()
         self.playlist = Pys2Playlist()
-        self.model = PlaylistModel(self.playlist)
 
-        self.progress_mousedown = False
-        self.view = PlayerView(self.model)
-        self.connectUI()
+        self.seek_bar_pressed = False
 
-    def connectUI(self):
-        self.view.progress.sliderPressed.connect(self.on_progress_mouseup)
-        self.view.progress.sliderReleased.connect(self.on_progress_mousedown)
-        self.view.progress.valueChanged.connect(self.on_progress_changed)
+        log.debug("PlayerController initialzied")
 
-        self.view.prev_btn.clicked.connect(self.on_prev_clicked)
-        self.view.play_btn.clicked.connect(self.on_play_clicked)
+        self.connect()
+
+    def connect(self):
+
+        connection = self.view.prv_btn.clicked.connect(self.playlist.previous)
+
+        self.view.prv_btn.clicked.connect(self.on_prv_clicked)
+        self.view.ply_btn.clicked.connect(self.on_play_clicked)
         self.view.pause_btn.clicked.connect(self.on_pause_clicked)
         self.view.stop_btn.clicked.connect(self.on_stop_clicked)
         self.view.next_btn.clicked.connect(self.on_next_clicked)
 
-        self.view.add_file_btn.clicked.connect(self.on_add_song_clicked)
+        self.view.seek_bar.sliderPressed.connect(self.on_seek_mousedown)
+        self.view.seek_bar.sliderReleased.connect(self.on_seek_mouseup)
+        self.view.seek_bar.valueChanged.connect(self.on_seek_change)
 
-    @QtCore.Slot()
-    def on_progress_mouseup(self):
-        log.debug("on_progress_mouseup")
-        self.progress_mousedown = False
+        self.view.add_file_btn.clicked.connect(self.do_add_file)
+        self.view.add_dir_btn.clicked.connect(self.do_add_directory)
 
-    @QtCore.Slot()
-    def on_progress_mousedown(self):
-        log.debug("on_progress_mousedown")
-        self.progress_mousedown = True
+        self.view.randomize_btn.clicked.connect(self.on_randomize_click)
+        self.view.sequential_btn.clicked.connect(self.on_sequential_click)
 
-    @QtCore.Slot()
-    def on_progress_changed(self):
-        if self.progress_mousedown is True:
-            if self.playlist.current is not None:
-                self.playlist.current.move2position_bytes(self.view.progress.value())
+        #Playlist connections
+        self.playlist.ticked.connect(self.on_pl_tick)
+        self.playlist.song_changed.connect(self.on_song_changed)
 
-    @QtCore.Slot()
-    def on_prev_clicked(self):
+        log.debug("PlayerController connected to view")
+
+    def on_prv_clicked(self):
+        log.debug("PlayerController.on_prv_clicked")
         self.playlist.previous()
 
-
-    @QtCore.Slot()
     def on_play_clicked(self):
+        log.debug("PlayerController.on_play_clicked")
         self.playlist.play()
 
-    @QtCore.Slot()
     def on_pause_clicked(self):
+        log.debug("PlayerController.on_pause_clicked")
         self.playlist.pause()
 
-    @QtCore.Slot()
     def on_stop_clicked(self):
+        log.debug("PlayerController.on_stop_clicked")
         self.playlist.stop()
 
-    @QtCore.Slot()
     def on_next_clicked(self):
+        log.debug("PlayerController.on_next_clicked")
         self.playlist.next()
 
-    @QtCore.Slot()
-    def on_add_song_clicked(self):
-        fileDialog = QtWidgets.QFileDialog(self.view)
-        supportedMimeTypes = ['application/octet-stream']
-        fileDialog.setMimeTypeFilters(supportedMimeTypes)
-        if fileDialog.exec_() == QtWidgets.QDialog.Accepted:
-            files = fileDialog.selectedFiles()
-            file = files[0]
-            # Assure the song loads correctly
-            try:
-                index, song = self.playlist.add_song(file)
-                song.handle is not None
-                song.free_stream()
-            except BassException as bexc:
-                box = QtWidgets.QMessageBox()
-                box.setText(f"Unable to load {file=} because {bexc.desc}")
-                box.exec_()
-            else:
+    def on_seek_mousedown(self):
+        self.seek_bar_pressed = True
 
-                song.position_updated.connect(self.on_progress_changed)
-                seconds = song.duration
-                self.view.progress.setRange(0, seconds)
-                self.view.duration.setText(str(seconds))
-                self.model.add_song(index, song)
+    def on_seek_mouseup(self):
+        self.seek_bar_pressed = False
+
+    def on_seek_change(self, value):
+        if self.seek_bar_pressed is True and self.playlist.current is not None:
+            self.playlist.current.move2position_bytes(value)
+
+
+    def on_pl_tick(self):
+        self.do_state_update()
+
+    def on_song_changed(self, song_id):
+        log.debug("PlayerController.on_song_changed")
+        song = self.playlist.get_song_by_id(song_id)
+        if song is not None:
+            log.debug("new song position to duration - %s to %s",
+                      song.position_bytes,
+                      song.duration_bytes
+                      )
+            self.view.seek_bar.setRange(0, song.duration_bytes)
+            self.view.seek_bar.setValue(song.position_bytes)
+
+    def do_state_update(self):
+        # Add 1 to queue position because it starts counting from 0
+        self.view.pl_position.setText(f"{self.playlist.queue_position+1}/{len(self.playlist.queue)}({len(self.playlist)})")
+        if self.playlist.current is not None:
+            song_name = self.playlist.current.file_path.name
+            song_pos = self.playlist.current.position_time
+            song_len = self.playlist.current.duration_time
+            song_val = self.playlist.current.position_bytes
+        else:
+            song_name = "Nothing playing"
+            song_pos = "0:00"
+            song_len = "0:00"
+            song_val = 0
+
+        self.view.song_title.setText(song_name)
+        self.view.song_position.setText(song_pos)
+        self.view.song_duration.setText(song_len)
+        self.view.seek_bar.setValue(song_val)
+
+
+    def do_add_file(self):
+        log.debug("PlayerController.do_add_file")
+
+        name_filters = [
+            "Audio files (*.wav *.mp3 *.mp4 *.ogg)",
+            "Any files (*)"
+        ]
+
+        music_dir = (pathlib.Path.home() / "music").as_posix()
+
+        song_file = QtWidgets.QFileDialog.getOpenFileName(
+            self.view,
+            "Select song",
+            music_dir,
+            ";;".join(name_filters)
+        )
+
+        log.debug("User chose %s", song_file)
+
+        if song_file:
+            song_file = pathlib.Path(song_file[0])
+
+            if song_file.exists() and song_file.is_file():
+                song_id, song = self.playlist.add_song(song_file)
+                self.do_state_update()
+                self.playlist.play_song(song_id)
+
+
+    def do_add_directory(self):
+
+        log.debug("PlayerController.do_add_directory")
+        music_dir = (pathlib.Path().home() / "music").as_posix()
+
+        directory = QtWidgets.QFileDialog.getExistingDirectory(self.view,
+                                                               "Select directory",
+                                                               music_dir,
+                                                               QtWidgets.QFileDialog.ShowDirsOnly
+                                                               )
+
+        dir_path = pathlib.Path(directory)
+
+        log.debug("User selected %s", dir_path)
+
+        if dir_path.exists():
+            self.playlist.add_directory(dir_path, recurse=True)
+            if len(self.playlist) > 0:
                 self.playlist.play()
 
+        self.do_state_update()
+
+    def on_randomize_click(self):
+        self.playlist.stop()
+
+        self.playlist.set_randomize()
+
+        if len(self.playlist) > 0:
+            self.playlist.queue_position = 0
+            self.playlist.play()
+
+    def on_sequential_click(self):
+        self.playlist.stop()
+
+        self.playlist.set_sequential()
+
+        if len(self.playlist) > 0:
+            self.playlist.queue_position = 0
+            self.playlist.play()
 
 
+def setup_logging():
 
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-class PlayerView(QtWidgets.QMainWindow):
-
-    def __init__(self, model: PlaylistModel):
-        super(PlayerView, self).__init__()
-        self.model = model
-
-        self.setupUI()
-        self.configureTable(model)
-
-    def setupUI(self) -> None:
-        """
-            Layout sketch
-            -------------
-
-            Song file name
-            position text / duration text
-            <previous> <play> <pause> <stop> <next>
-            [Play list table]
-
-        :return:
-        """
-
-        body = QtWidgets.QFrame()
-        vbox = QtWidgets.QVBoxLayout()
-
-        self.song_title = QtWidgets.QLabel("Song title")
-        vbox.addWidget(self.song_title)
-
-        stat_line = QtWidgets.QHBoxLayout()
-        self.position = QtWidgets.QLabel("0:00")
-        stat_line.addWidget(self.position)
-
-        self.progress = QtWidgets.QSlider(Qt.Horizontal)
-        stat_line.addWidget(self.progress)
-
-        self.duration = QtWidgets.QLabel("0:00")
-        stat_line.addWidget(self.duration)
-
-
-        vbox.addLayout(stat_line)
-
-        cmd_line = QtWidgets.QHBoxLayout()
-        self.prev_btn = QtWidgets.QPushButton("Previous")
-        cmd_line.addWidget(self.prev_btn)
-
-        self.play_btn = QtWidgets.QPushButton("Play")
-        cmd_line.addWidget(self.play_btn)
-
-        self.pause_btn = QtWidgets.QPushButton("Pause")
-        cmd_line.addWidget(self.pause_btn)
-
-        self.stop_btn = QtWidgets.QPushButton("Stop")
-        cmd_line.addWidget(self.stop_btn)
-
-        self.next_btn = QtWidgets.QPushButton("Next")
-        cmd_line.addWidget(self.next_btn)
-
-        vbox.addLayout(cmd_line)
-
-        self.table = QtWidgets.QTableView()
-        vbox.addWidget(self.table)
-
-        play_list_ctrls = QtWidgets.QHBoxLayout()
-        self.randomize_btn = QtWidgets.QPushButton("Sequential")
-        self.randomize_btn.setCheckable(True)
-        play_list_ctrls.addWidget(self.randomize_btn)
-
-        self.repeat_btn = QtWidgets.QPushButton("Loop")
-        self.repeat_btn.setCheckable(True)
-        play_list_ctrls.addWidget(self.repeat_btn)
-
-        self.clear_btn = QtWidgets.QPushButton("Clear")
-        play_list_ctrls.addWidget(self.clear_btn)
-
-        self.add_file_btn = QtWidgets.QPushButton("Add song")
-        play_list_ctrls.addWidget(self.add_file_btn)
-
-        self.add_dir_btn = QtWidgets.QPushButton("Add directory")
-        play_list_ctrls.addWidget(self.add_dir_btn)
-
-        vbox.addLayout(play_list_ctrls)
-
-        body.setLayout(vbox)
-        self.setCentralWidget(body)
-
-    def configureTable(self, playlist_obj):
-
-        self.table.setModel(self.model)
-        self.table.setColumnHidden(0, True)
-        self.table.verticalHeader().hide()
-
-        self.model.record_added.connect(self.on_new_song)
-
-    def on_new_song(self, rid = None):
-        log.debug("Song added successfully, queuing to play %s", rid)
-        if rid:
-            pass
-
-
-        self.table.resizeColumnsToContents()
+    pass
 
 def main():
-
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format="%(levelname)s:%(name)s:%(funcName)s@%(lineno)s - %(message)s")
-    log.info("Starting")
+    setup_logging()
+    log.info("Example Pyside2 player started!")
 
     app = QtWidgets.QApplication()
-    control = PlayerControl()
-    control.view.show()
+    player = PlayerController()
+    player.view.show()
 
     app.exec_()
-    log.info("Shutting down")
+
 
 if __name__ == "__main__":
     main()
