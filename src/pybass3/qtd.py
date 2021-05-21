2c9dd2cdb9403711a2cf3b5c84d63dd19a232a4f
devdave <devdave@ominian.net>
2021-05-21 08:07:35 -0600

    Organize a little bit better and add additional import for Qt an QtCore

--- src/pybass3/qtd.py
+++ src/pybass3/qtd.py
@@ -3,7 +3,7 @@
 """
 
 try:
-    from PySide2.QtCore import QObject, QTimer
-    from PySide2.QtCore import Signal
+    from PySide2 import QtCore
+    from PySide2.QtCore import QObject, QTimer, Signal, Qt
 except ImportError:
     raise NotImplementedError("Missing Pyside2 and PyQT5 imports not implemented yet")
